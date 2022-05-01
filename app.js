3e104f61917f8ad2495f5b7242f514ce8b8f6bfa
Goltran Mazon <goltran_mazon@hotmail.com>
2022-05-01 01:48:03 -0700

    Commit lines

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function computerPlay() {
+    //Get random selection from computer
     let option = ['rock', 'paper', 'scissers'];
     return option[Math.floor(Math.random() * 3)];
 }
@@ -6,6 +7,7 @@ function computerPlay() {
 
 
 function playerPlay() {
+    //Get selection from player
     let player = prompt('Rock, Paper or Scissors?');
     return player.toLowerCase();
 }
@@ -26,20 +28,21 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+//Initialize round counters
 let playerRounds = 0;
 let computerRounds = 0;
 
 for (let i = 1; i <= 5; i++) {
-    // your code here!
+    // Repeat round 5 times
     console.log('Round ' + i);
     const playerSelection = playerPlay();
     if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+        //if player selection is wrong, repeat round
         console.log('Please select Rock, Paper or Scissors.')
         i--;
         continue;
     }
     const computerSelection = computerPlay();
-
     let round = playRound(playerSelection, computerSelection);
     switch (round) {
         case 1:
@@ -56,7 +59,7 @@ for (let i = 1; i <= 5; i++) {
             break;
     }
 }
-console.log('Fin ' + playerRounds + ' - ' + computerRounds);
+console.log('Final ' + playerRounds + ' - ' + computerRounds);
 if (playerRounds > computerRounds) {
     console.log('You Win this Game!');
 } else {
